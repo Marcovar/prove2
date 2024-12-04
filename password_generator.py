cabb26641455d5272426390935cc7a8f66a044f5
Marcovar <elvara@hotmail.it>
2024-12-04 20:09:25 +0100

    modificato

--- password_generator.py
+++ password_generator.py
@@ -4,16 +4,21 @@
 #     -la lunghezza sarà l'input numerico dall'utente
 
 #guardate random.choiche()
-
 import string
 import random
-lunghezza = input("Quanti caratteri vuoi nella tua password?")
-def Password(lunghezza,lista_caratteri):
-    lista_caratteri = random.choice(["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","z","?","!","^","_","@","$","%"])
+lunghezza = int(input("Quanti caratteri vuoi nella tua password? "))
+def Password(lunghezza):
+    lista_caratteri = []
+    lista_caratteri.append(random.choice("0123456789"))
+    lista_caratteri.append(random.choice(string.ascii_letters))
+    lista_caratteri.append(random.choice("?!^_@$%"))
+    while len(lista_caratteri) < lunghezza:
+        lista_caratteri.append(random.choice(string.ascii_letters + "0123456789" + "?!^_@$%"))
+        
+    random.shuffle(lista_caratteri)
 
-    numero_lettere = int(lunghezza) - len(lista_caratteri)
-    for index in range(numero_lettere):
-        lista_caratteri.append(random.choice(string.ascii_letters))
-        random.shuffle(lista_caratteri)
-        password = "".join(lista_caratteri)
-return Password
+    password = "".join(lista_caratteri)
+    return password
+
+password_generata = Password(lunghezza)
+print(f"La tua password generata è: {password_generata}")
